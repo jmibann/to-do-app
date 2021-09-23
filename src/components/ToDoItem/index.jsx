a879c7c57df16dea7e3151c7310cc24ec42995b6
Juan Manuel <juanmanuel@juani.local>
2021-09-23 12:49:24 -0300

    Styles items button

--- src/components/ToDoItem/index.jsx
+++ src/components/ToDoItem/index.jsx
@@ -4,8 +4,14 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCheck, faUndoAlt } from '@fortawesome/free-solid-svg-icons'
 
 const ToDoItem = ({ children, completed = false, toggleTaskStatus }) => {
-  const itemBaseClass = `w-full my-2 p-2 rounded flex items-center justify-between`;
+  const itemBaseClass = 'w-full my-2 p-2 rounded flex items-center justify-between';
   const itemClass = itemBaseClass.concat(` ${completed ? 'bg-green-300' : 'bg-red-300'}`);
+
+  const buttonBaseClass = 'cursor-pointer py-1 px-2 border-2 border-gray-600 rounded-full border-opacity-50 shadow-md';
+  const buttonClass = buttonBaseClass.concat(` ${completed
+    ? 'bg-green-300 hover:border-red-500 hover:bg-red-300'
+    : 'bg-red-300 hover:border-green-500 hover:bg-green-300'}`
+  );
 
   return (
     <div className={itemClass}>
@@ -14,7 +20,7 @@ const ToDoItem = ({ children, completed = false, toggleTaskStatus }) => {
       </span>
 
       <div className="flex items-center justify-around w-2/12 h-full">
-        <div className="cursor-pointer p-1" onClick={toggleTaskStatus}>
+        <div className={buttonClass} onClick={toggleTaskStatus}>
           <FontAwesomeIcon icon={completed ? faUndoAlt : faCheck} />
         </div>
       </div>
